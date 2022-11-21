39f6d48c0fa374cccd0a64f1e03301374681a9ea
1185101290@qq.com <1185101290@qq.com>
2022-11-21 21:48:54 +0800

    start

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import "./wdyr";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { DevTools, loadServer } from "jira-dev-tool";
+// 务必在jira-dev-tool后面引入
+import "antd/dist/antd";
+
+
+loadServer(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+     
+          <DevTools />
+          <App />
+       
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
